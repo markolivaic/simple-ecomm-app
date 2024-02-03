313cb5e5dd22d9ae975ece6ab36cd57d1e9caf6a
Francesco Marko Livaic <makifrane@gmail.com>
2024-02-03 22:03:51 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {lazy, Suspense} from 'react'
+import Navbar from "./Components/Navbar/Navbar"
 
+const Main = lazy(() => import("./Pages/Main/Main"))
 
 function App() {
   return (
-    <></>
+    <Router>
+      <Navbar />
+      <Suspense fallback={<p>Loading</p>}>
+        <Routes>
+          <Route path='' element={<Main />} />
+        </Routes>
+      </Suspense>
+    </Router>
   )
 }
 
---
+++ src/Pages/Main/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Main.module.scss'
+
+const Main = () => {
+    return(
+        <div></div>
+    )
+}
+
+export default Main
